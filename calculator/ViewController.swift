159debeda1e89c6999302c01b228744ee9513461
Damian Espada <Damian7077@gmail.com>
2023-02-13 09:50:48 -0700

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Sosa Espada, Damian - Student on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    //declaring objects and variable
+    @IBOutlet weak var text1: UITextField!
+    var text1String=""
+    var text11:Double = 0.0
+    
+    //=>
+    @IBOutlet weak var text2: UITextField!
+    var text2String=""
+    var text22:Double = 0.0
+    var answerDouble = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    @IBAction func button(_ sender: Any) {
+        text1String = text1.text ?? ""
+        text2String = text2.text ?? ""
+        text11 = Double(text1String) ?? 0.0
+        text22 = Double(text2String) ?? 0.0
+        // letting the text to be receved when you press the button.
+        //?? is an optional It provides a default valiue in case the user dosent enter one.
+        answerDouble = text11 * text22
+        
+        label.text="\(answerDouble)"
+        
+        
+        
+        }
+  
+        
+        
+    
+    
+}
